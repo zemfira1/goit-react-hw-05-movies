343f857cb51a12335c4d9d7afcc5b79efa2047e6
zemfira1 <craftyzema@gmail.com>
2023-08-22 12:35:59 -0400

    add loadMore button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,36 +3,45 @@ import { getTrendingMovies } from '../../servises/api';
 import { ListOfMovies } from 'components/ListOfMovies';
 import { Loader } from 'components/Loader';
 import { Title } from './Home.styled';
+import { LoadMore } from 'components/LoadMore';
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isButton, setIsButton] = useState(false);
 
   useEffect(() => {
-    const getTrendingMoviesData = async () => {
-      try {
-        setIsLoading(true);
-        setMovies(await getTrendingMovies());
-      } catch (error) {
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+    setIsLoading(true);
 
-    getTrendingMoviesData();
-  }, []);
+    getTrendingMovies(currentPage)
+      .then(r => {
+        if (r.results.length === 0) {
+          alert('Sorry, there are no movies.');
+          return;
+        }
+        setMovies(prevState => [...prevState, ...r.results]);
+        setIsButton(currentPage < Math.ceil(r.total_results / 20));
+      })
+      .catch(error => setError(error))
+      .finally(() => setIsLoading(false));
+  }, [currentPage]);
+
+  const loadMoreMovies = () => {
+    setCurrentPage(prevState => prevState + 1);
+  };
 
   return (
     <>
       {isLoading && <Loader />}
       {movies.length !== 0 && (
         <>
-          <Title>Trending today</Title>
+          <Title>Trending this week</Title>
           <ListOfMovies movies={movies} />
         </>
       )}
+      {isButton && <LoadMore onClick={loadMoreMovies} />}
       {error && alert('Sorry, something is wrong!')}
     </>
   );
--- src/servises/api.js
+++ src/servises/api.js
@@ -5,10 +5,15 @@ axios.defaults.headers.common['Authorization'] =
   'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NzhhYjBlYjNmYjE0YzA0ODc2YzI1YmIxNDI0M2NiZCIsInN1YiI6IjY0ZDE2MzYyNmQ0Yzk3MDBlYzU4YjNlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wkZrfHNayLG0He9kinamYKtKq9Py8-TCiR3q_k01mNs';
 axios.defaults.headers.common['accept'] = 'application/json';
 
-export const getTrendingMovies = async () => {
-  const { data } = await axios.get('trending/movie/day');
+export const getTrendingMovies = async page => {
+  const { data } = await axios.get('trending/movie/week', {
+    params: {
+      page: page,
+      per_page: 20,
+    },
+  });
 
-  return data.results;
+  return data;
 };
 
 export const getMovieDetails = async movieId => {
